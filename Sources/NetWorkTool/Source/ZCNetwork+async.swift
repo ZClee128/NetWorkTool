480c466ff4bf95a6756ad868e2c0ef38d0f787fd
lee <leecong@miaoshitech.com>
2024-01-15 14:36:48 +0800

    [main]fix 版本号

--- Sources/NetWorkTool/Source/ZCNetwork+async.swift
+++ Sources/NetWorkTool/Source/ZCNetwork+async.swift
@@ -5,7 +5,7 @@ import Moya
 public extension ZCNetwork {
     func requestAsync<D:Decodable>(api: TargetType & ZCApiProtocol) async -> ZCResult<D,ZCNetworkError> {
          await withCheckedContinuation { continuation in
-            self.request(api: api) { (result: WEResult<D,WGNetworkError>) in
+            self.request(api: api) { (result: ZCResult<D,ZCNetworkError>) in
                 continuation.resume(returning: result)
             }
         }
